a1eb8b8f9a9e6561b23e72850e1a7b3f6c507a00
Michael Dave Sumang <mdsumang.091@gmail.com>
2024-10-24 20:35:09 +0800

    implement insertInstructor() and findInstructor()

--- src/main/java/org/acumen/training/codes/dao/TeacherDao.java
+++ src/main/java/org/acumen/training/codes/dao/TeacherDao.java
@@ -1,10 +1,18 @@
 package org.acumen.training.codes.dao;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.acumen.training.codes.model.Instructor;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 import org.jboss.logging.Logger;
+
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
 
 public class TeacherDao {
 	private static final Logger LOGGER = Logger.getLogger(TeacherDao.class);
@@ -47,5 +55,95 @@ public class TeacherDao {
 			}
 		}
 		return false;
+	}
+	
+	public boolean deleteInstructor(String id) {
+		LOGGER.info("executing deleteInstructor()...");
+		Session sess = sf.openSession();
+		Transaction txn = sess.beginTransaction();
+
+		try {
+			Instructor instructor = sess.get(Instructor.class, id); 
+			sess.remove(instructor);
+			txn.commit();
+			LOGGER.info("deleteInstructor() executed successfully");
+			return true;
+		} catch (Exception e) {
+			try {
+				txn.rollback();
+			} catch (Exception ee) {
+				LOGGER.error("caught an exception deleteInstructor(): %s"
+						.formatted(ee.getMessage()));
+				ee.printStackTrace();
+			}
+			LOGGER.error("caught an exception deleteInstructor(): %s"
+					.formatted(e.getMessage()));
+			e.printStackTrace();
+		} finally {
+			try {
+				sess.close();
+			} catch (Exception eee) {
+				LOGGER.error("caught an exception deleteInstructor(): %s"
+						.formatted(eee.getMessage()));
+				eee.printStackTrace();
+			}
+		}
+		return false;
+	}
+	
+	public boolean insertInstructor(Instructor instructor) {
+		LOGGER.info("executing insertInstructor()...");
+		Session sess = sf.openSession();
+		Transaction txn = sess.beginTransaction();
+		try {
+			sess.persist(instructor);
+			txn.commit();
+
+			LOGGER.info("insertInstructor() executed successfully");
+			return true;
+		} catch (Exception e) {
+			try {
+				txn.rollback();
+			} catch (Exception ee) {
+				LOGGER.error("caught an exception insertInstructor(): %s"
+						.formatted(ee.getMessage()));
+				ee.printStackTrace();
+			}
+			LOGGER.error("caught an exception insertInstructor(): %s"
+					.formatted(e.getMessage()));
+			e.printStackTrace();
+		} finally {
+			try {
+				sess.close();
+			} catch (Exception eee) {
+				LOGGER.error("caught an exception insertInstructor(): %s"
+						.formatted(eee.getMessage()));
+				eee.printStackTrace();
+			}
+		}
+		return false;
+	}
+	
+	public List<Instructor> findInstructor(String course) {
+		LOGGER.info("executing findInstructor()...");
+		List<Instructor> recs = new ArrayList<>();
+		
+		try (Session sess = sf.openSession();) {
+			CriteriaBuilder builder = sess.getCriteriaBuilder();
+			CriteriaQuery<Instructor> sql = builder.createQuery(Instructor.class);
+			Root<Instructor> from = sql.from(Instructor.class);
+			
+			sql.select(from).where(builder.equal(from.get("deptName"), course));
+
+			Query<Instructor> query = sess.createQuery(sql);
+			
+			LOGGER.info("findInstructor() executed successfully");
+			return recs = query.getResultList();
+		} catch (Exception e) {
+			LOGGER.error("caught an exception findInstructor(): %s"
+					.formatted(e.getMessage()));
+			e.printStackTrace();
+		}
+		return recs;
 	}
 }
--- src/test/java/org/acumen/training/codes/test/TestTeacherDao.java
+++ src/test/java/org/acumen/training/codes/test/TestTeacherDao.java
@@ -1,8 +1,12 @@
 package org.acumen.training.codes.test;
 
+import java.util.List;
+
 import org.acumen.training.codes.UnivConfiguration;
+import org.acumen.training.codes.dao.StudentDao;
 import org.acumen.training.codes.dao.TeacherDao;
 import org.acumen.training.codes.model.Instructor;
+import org.acumen.training.codes.model.Student;
 import org.hibernate.SessionFactory;
 import org.jboss.logging.Logger;
 import org.junit.jupiter.api.AfterEach;
@@ -44,5 +48,41 @@ public class TestTeacherDao {
 
 		dao.updateSalary("12121", 65000.0);
 		LOGGER.info("testUpdateSalary() executed");
+	}
+	
+	@Disabled
+	@Test
+	public void testDeleteInstructor() {
+		cfg.createConfiguration();
+		SessionFactory sf = cfg.getSessionFactory();
+		TeacherDao dao = new TeacherDao(sf);
+
+		dao.deleteInstructor("12345");
+		LOGGER.info("testUpdateSalary() executed");
+	}
+	
+	@Disabled
+	@Test
+	public void testInsertInstructor() {
+		cfg.createConfiguration();
+		SessionFactory sf = cfg.getSessionFactory();
+		TeacherDao dao = new TeacherDao(sf);
+		
+		dao.insertInstructor(createInstructor("12345","Sumang","Music",65000.00));
+		dao.insertInstructor(createInstructor("21222","Walter","Comp. Sci.",75000.00));
+		dao.insertInstructor(createInstructor("01012","Smith","Aviation",75000.00));
+		LOGGER.info("testInsertInstructor() executed");
+	}
+	
+	@Test
+	public void testFindInstructor() {
+		cfg.createConfiguration();
+		SessionFactory sf = cfg.getSessionFactory();
+		TeacherDao dao = new TeacherDao(sf);
+		
+		List<Instructor> data = dao.findInstructor("Music");
+		
+		System.out.println(data.toString());
+		LOGGER.info("testFindInstructor() executed");
 	}
 }
